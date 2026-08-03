afaa31f1d832bb64f1e88eb815aeb67b1382f77d
yasin <yasineyimaya700@gmail.com>
2026-08-03 14:33:21 +0300

    feat: Gizlilik Politikasi ve Kullanim Sartlari bolumleri eklendi, linkler anchor'a guncellendi

--- public/script_v105.js
+++ public/script_v105.js
@@ -540,9 +540,9 @@ function initCookieConsent() {
         '</div>' +
         '<p class="ccb-desc">Web sitemizde deneyiminizi geliştirmek için çerezler kullanıyoruz. Detaylı bilgi için gizlilik politikamızı inceleyebilirsiniz.</p>' +
         '<div class="ccb-links">' +
-            '<a href="/about">Gizlilik Politikası</a>' +
+            '<a href="/about#gizlilik">Gizlilik Politikası</a>' +
             '<span class="ccb-link-dot">•</span>' +
-            '<a href="/about">Kullanım Şartları</a>' +
+            '<a href="/about#kullanim-sartlari">Kullanım Şartları</a>' +
         '</div>' +
         '<div class="ccb-actions">' +
             '<div class="ccb-actions-primary">' +
